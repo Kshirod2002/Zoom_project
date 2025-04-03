16a03e76a173ce65befc5edfc4f3600f0554c234
Kshirod  Chandra Swain <swainkunal164@gmail.com>
2025-04-03 11:22:02 +0530

    Update VideoMeet.jsx

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -11,10 +11,8 @@ import MicOffIcon from '@mui/icons-material/MicOff'
 import ScreenShareIcon from '@mui/icons-material/ScreenShare';
 import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
-//import server from '../environment';
 
-//const server_url = server;
-const server_url = "http://localhost:8000";
+const server_url = "https://zoom-project-backend-8kc5.onrender.com";
 var connections = {};
 
 const peerConfigConnections = {
